a14a5fdb2c3a1198a909add9b11606f215edc632
youknow92 <dbflwla1@gmail.com>
2020-05-24 00:47:15 +0900

    게시판 UI 추가 및 등록 / 수정 / 삭제 기능 추가

--- src/main/java/com/youknow/book/springboot/service/PostsService.java
+++ src/main/java/com/youknow/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.youknow.book.springboot.service;
 
 import com.youknow.book.springboot.domain.posts.Posts;
 import com.youknow.book.springboot.domain.posts.PostsRepository;
+import com.youknow.book.springboot.web.dto.PostsListResponseDto;
 import com.youknow.book.springboot.web.dto.PostsResponseDto;
 import com.youknow.book.springboot.web.dto.PostsSaveRequestDto;
 import com.youknow.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,6 +37,21 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(()->new IllegalArgumentException("해당 사용자가 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) //트렌젝션 범위는 유지하되 조회 기능만 남겨놓는다.
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 
 }
