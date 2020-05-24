ceb618be23fb3a28314f3f04e89bbff492dfb964
youknow92 <dbflwla1@gmail.com>
2020-05-24 22:29:45 +0900

    OAuth 적용 후 테스트 작동 안되는 부분 개선

--- src/main/java/com/youknow/book/springboot/Application.java
+++ src/main/java/com/youknow/book/springboot/Application.java
@@ -2,9 +2,8 @@ package com.youknow.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing //JPA Auditing 활성화
+//@EnableJpaAuditing JpaConfig로 이동
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
